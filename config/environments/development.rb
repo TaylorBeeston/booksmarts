7b82eac257a5dc4576c40fb090e12ca00f59b1cf
TaylorBeeston <beeston.taylor@gmail.com>
2019-09-02 04:41:13 -0700

    Add Devise/Figaro and set up mailers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,23 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # ActionMailer config
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.deliver_method = :smtp
+  config.action_mailer.default charset: 'utf-8'
+  config.action_mailer.default_options = { from: ENV['GMAIL_USERNAME'] }
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    openssl_verify_mode: 'none'
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,6 +39,20 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_options = { from: ENV['GMAIL_USERNAME'] }
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 25,
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    domain: ENV['DOMAIN_NAME'],
+    openssl_verify_mode: 'none'
+  }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
