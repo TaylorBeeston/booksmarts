931d8c9ffc1effb8855108a3cc3679466cea6c33
TaylorBeeston <beeston.taylor@gmail.com>
2019-09-08 12:54:13 -0700

    Pass Book tests

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,18 +1,29 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
+  def setup
+    @valid  = books(:valid)
+    @noauth = books(:noauthor)
+  end
+
   test 'should not save without title' do
     book = Book.new
-    assert !book.save
+    assert_not book.valid?
   end
 
   test 'should not save without a user' do
     book = Book.new(title: 'Test')
-    assert !book.save
+    assert_not book.valid?
   end
 
   test 'author should default to unknown' do
     book = Book.new
-    assert book[:author] = 'Unknown'
+    assert_equal 'Unknown', book[:author]
+  end
+
+  test 'valid book should save even with no author' do
+    assert_equal 'This is a Valid Book', @valid[:title]
+    assert @valid.valid?, 'Valid Book is not valid'
+    assert @noauth.valid?, 'Book with no author is not valid'
   end
 end
