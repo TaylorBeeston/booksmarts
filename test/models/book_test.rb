6758e91ea2af53acc7817ec5e7c8142c64fc0a5b
TaylorBeeston <beeston.taylor@gmail.com>
2019-09-08 08:16:17 -0700

    Write failing Book model tests

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save without title' do
+    book = Book.new
+    assert !book.save
+  end
+
+  test 'should not save without a user' do
+    book = Book.new(title: 'Test')
+    assert !book.save
+  end
+
+  test 'author should default to unknown' do
+    book = Book.new
+    assert book[:author] = 'Unknown'
+  end
 end
