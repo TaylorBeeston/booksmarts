1eb44c92604196fbf213e7de5da3ce0baaa3295e
TaylorBeeston <beeston.taylor@gmail.com>
2019-09-14 09:14:17 -0700

    Add route/controller action for adding a Book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -64,6 +64,10 @@ class BooksController < ApplicationController
     end
   end
 
+  # POST /books/add
+  def add
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_book
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :books
+  resources :books do
+    member do
+      post 'add'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'static_pages#index'
