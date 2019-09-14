a32d39b59d223d2360105795ba0fb6741c606fa9
TaylorBeeston <beeston.taylor@gmail.com>
2019-09-14 09:50:41 -0700

    Add in_library? helper for Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,10 @@ class Book < ApplicationRecord
   validates_presence_of :title
   before_update :default_author
 
+  def in_library?(user)
+    user.books.include? self
+  end
+
   private
 
     def default_author
