bd560c231c86e61199ae22fadfb2d872d84a3907
TaylorBeeston <beeston.taylor@gmail.com>
2019-10-02 08:14:35 -0700

    Update User model to allow users to reccover their password with their username

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,14 +15,17 @@ class User < ApplicationRecord
     @login || self.username || self.email
   end
 
-  def self.find_for_database_authentication(warden_conditions)
+  def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if (login = conditions.delete(:login))
       where(conditions.to_h).where(['lower(username) = :value OR lower(email = :value)',
                                     { value: login.downcase }]).first
-    elsif conditions.key?(:username) || conditions.key?(:email)
-      conditions[:email].downcase! if conditions[:email].exists?
-      where(conditions.to_h).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first 
+      else
+        where(username: conditions[:username]).first
+      end
     end
   end
 
