f292ed13416a470b3709a5263b7623f1511773b2
TaylorBeeston <beeston.taylor@gmail.com>
2019-10-02 08:03:54 -0700

    Add username validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :books
   attr_writer :login
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
