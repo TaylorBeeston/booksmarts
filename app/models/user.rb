bfba815b8a1287be762c8aaac18e2110e8ce092c
TaylorBeeston <beeston.taylor@gmail.com>
2019-10-02 00:36:34 -0700

    Update User Model to accept both username and email for login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
   has_many :books
+  attr_writer :login
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable
+
+  def login
+    @login || self.username || self.email
+  end
 end
