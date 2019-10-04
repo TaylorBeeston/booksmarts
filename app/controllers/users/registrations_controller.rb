f267db0e21c313e6088bb744361ad881756315ef
TaylorBeeston <beeston.taylor@gmail.com>
2019-10-04 00:38:42 -0700

    Add three default Libraries on User Registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,25 +11,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-    build_resource(sign_up_params)
-
-    resource.save
-    yield resource if block_given?
-    if resource.persisted?
-      if resource.active_for_authentication?
-        set_flash_message! :notice, :signed_up
-        sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
-      else
-        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-        expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
-      end
-    else
-      clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource
-    end
+    super
+    Library.create(name: 'Books I have Read', user_id: resource.id)
+    Library.create(name: 'Books I want to Read', user_id: resource.id)
+    Library.create(name: 'Books I am Reading', user_id: resource.id)
   end
 
   # GET /resource/edit
